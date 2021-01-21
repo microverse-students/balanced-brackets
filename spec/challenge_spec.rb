79f0420e04d575cf3a9c59c5ec2188aaf14e4fd9
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2021-01-21 15:21:53 +0100

    Update challenge_spec.rb

--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -1,7 +1,41 @@
 require './challenge.rb'
 
 RSpec.describe 'UnitTests' do
-
   
+  it 'balanced_parenthesis_and_square_brackets' do
+    # Failure message: 
+    # Method called: balanced_brackets?('(hello)[world]')
+    expect(balanced_brackets?('(hello)[world]')).to be true
+  end
+  
+  it 'balanced_brackets' do
+    # Failure message: 
+    # Method called: balanced_brackets?('{}')
+    expect(balanced_brackets?('{}')).to be true
+  end
+  
+  it 'nested_balanced_brackets' do
+    # Failure message: 
+    # Method called: balanced_brackets?('[({}{}{})([])]')
+    expect(balanced_brackets?('[({}{}{})([])]')).to be true
+  end
 
+  it 'unbalanced_with_no_closing' do
+    # Failure message: 
+    # Method called: balanced_brackets?('(hello')
+    expect(balanced_brackets?('(hello')).to be false
+  end
+
+  it 'unbalanced_incorrect_nesting' do
+    # Failure message: 
+    # Method called: balanced_brackets?('([)]')
+    expect(balanced_brackets?('([)]')).to be false
+  end
+
+  it 'unbalanced_closing_before_opening' do
+    # Failure message: 
+    # Method called: balanced_brackets?(')(')
+    expect(balanced_brackets?(')(')).to be false
+  end
+  
 end
