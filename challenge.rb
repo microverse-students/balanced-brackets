20a363a1488ee31faa3cfb027df8ca6d95a912b4
Mauricio Santos <maosan132@gmail.com>
2021-10-13 21:51:54 -0500

    Update challenge.rb

--- challenge.rb
+++ challenge.rb
@@ -1,7 +1,21 @@
 
+
 def balanced_brackets?(string)
-  # your code here
-end
+  closing = ['[','{','(']
+  opening = [']','}',')']
+  hash= {"]"=>"[", "}"=>"{", ")"=>"("}
+  stack = []
+  regex = Regexp.union(opening+closing)
+  string.scan(regex).each do |char|
+    if closing.include? char
+      stack.push(char)
+    elsif opening.include? char
+      pop_val = stack.pop
+      return false if pop_val != open_close_hash[char]
+    end
+  end
+  closing.none? { |paren| stack.include? paren }
+end 
 
 puts balanced_brackets?('(hello)[world]')
 # => true
